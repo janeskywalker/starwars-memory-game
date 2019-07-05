047edd35292460900fcbb7f7bc0022eb2f158172
Jane Skywalker <janehuizeng@gmail.com>
2019-07-05 11:39:39 -0700

    adding flipCardBack methods to flip unmatched cards back to back

--- js/app.js
+++ js/app.js
@@ -31,34 +31,6 @@ const cards = [
 
 
 
-
-
-
-
-// --------------Game class---------------
-class Game {
-    constructor(cards) {
-        this.dealer = new Dealer(cards)
-        this.cardsFlipped = []
-        this.player1Score = 0
-        this.player2Score = 0
-        this.turn = 'Player One'
-        this.cardsOnBoard = []
-        this.matchCount = 0
-    }
-    
-
-    startGame() {
-        // add click eventListner to the div to flip card
-        const div = document.querySelector("#cardDisplay")
-        div.addEventListener('click', this.flipCard.bind(this))
-
-        this.dealer.shuffle()
-        console.log("cards after shuffled", this.dealer.cards)
-        this.cardsOnBoard = this.dealer.deal()
-        
-    }
-
     // play a turn
     // check for match
     // switch players
@@ -69,47 +41,113 @@ class Game {
 
 
 
-    //displayCards(cardsOnBoard) {
-        //console.log(cardsOnBoard)
+// --------------Game class---------------
+class Game {
+    constructor(cards) {
+        this.dealer = new Dealer(cards)
+        this.cardsFlipped = []
+        this.playerOneScore = 0
+        this.playerTwoScore = 0
+        this.turn = 'Player One'
+        this.cardsOnBoard = []
+        this.matchCount = 0
+    }
+    
+
+    startGame() {
+        // add click eventListner to the div to flip card
+        const div = document.querySelector("#cardDisplay")
+        // bind this back to the Game obj, instead of the element that 
+        // the eventListener is added on
+        div.addEventListener('click', this.playOneTurn.bind(this))
+
+        // get dealer to shuffle and deal
+        this.dealer.shuffle()
+        //console.log("cards after shuffled", this.dealer.cards)
+        this.cardsOnBoard = this.dealer.deal()
+        
+    }
+
 
     //play a turn 
-     flipCard (evt) {
-         console.log(this)
+    playOneTurn (evt) {
+         //console.log(this)
         if(evt.target.tagName === 'IMG' && this.cardsFlipped.length < 2) {
+
+            // did.id is the position on board
             console.log(evt.target.parentNode.id)
             let divClicked = document.getElementById(evt.target.parentNode.id)
             console.log(divClicked)
+
+            // flip images, to get the right images, using name
             divClicked.firstChild.setAttribute("src", `images/${this.cardsOnBoard[evt.target.parentNode.id].name}.jpg`)
 
+            // set data attribute to keep track on which div get flipped
+            // evt.target.dataset.parentId = evt.target.parentNode.id
+            // console.log(evt.target)
+        
             //add flipped cards into an array
-            this.cardsFlipped.push(this.cardsOnBoard[evt.target.parentNode.id])
+            // this.cardsFlipped.push(this.cardsOnBoard[evt.target.parentNode.id])
+            // console.log(this.cardsFlipped)
+
+            this.cardsFlipped.push({
+                flippedCard: this.cardsOnBoard[evt.target.parentNode.id], 
+                divId: evt.target.parentNode.id
+            })
             console.log(this.cardsFlipped)
 
-
-            this.checkMatching()
+            if(this.cardsFlipped.length === 2) {
+                this.checkMatching()
+            }
         }
     }
         
-
-    //}
-
     checkMatching() {
-        if(this.cardsFlipped.length>1) {
-            console.log(this.cardsFlipped)
+            console.log("cardsFlippedArray", this.cardsFlipped)
+            // if match, add score 1, check if all cards flipped 
+            if(this.cardsFlipped[0].flippedCard.name === this.cardsFlipped[1].flippedCard.name) {
+                console.log("you have got a match!")
+                // check which player, increment score 
+                if(this.turn === "Player One") {
+                    this.playerOneScore += 1
+                    console.log("playerOneScore:", this.playerOneScore)
+                } else if(this.turn === "Player Two") {
+                    this.playerTwoScore += 1
+                    console.log("playerTwoScore:", this.playerTwoScore)
+                }
 
-            // if match, add score 1
-            if(this.cardsFlipped[0].name === this.cardsFlipped[1].name) {
 
-                // check which player 
-                this.player1Score += 1
-                console.log(this.player1Score)
 
-                // if dont match, flip back 
+
+            // if dont match, flip back, then switch player
             } else { 
-                // switch user
+                console.log("no matching!")
+                setTimeout(() => {
+                    console.log('setTimeout')
+                    this.flipCardsBack()
+                }, 4000)
             }
         }
-        
+
+    flipCardsBack() {
+        console.log('flipping')
+        console.log("cardsFlippedArray:", this.cardsFlipped)
+
+        // why not able to log out the parentNode -- the div??
+
+        //find the parentNode id
+        // console.log(cardsFlipped[0].divId)
+        // console.log(cardsFlipped[1].divId)
+
+        const divId0 = this.cardsFlipped[0].divId
+        console.log(divId0)
+        let divToFlipBack0 = document.getElementById(divId0)
+        divToFlipBack0.firstChild.setAttribute("src", "images/back.svg")
+
+        const divId1 = this.cardsFlipped[1].divId
+        console.log(divId1)
+        let divToFlipBack1 = document.getElementById(divId1)
+        divToFlipBack1.firstChild.setAttribute("src", "images/back.svg")
     }
 
 
@@ -153,7 +191,7 @@ class Dealer {
 
     shuffle() {
         this.cards.sort(() => Math.random() - 0.5)
-        console.log("shuffledCards:", this.cards)
+        //console.log("shuffledCards:", this.cards)
     }
 
     deal() {
